93088403bbfc5ad75493ed412c9211d930fb59ae
Joseph Muigai <josephmuigai2012@gmail.com>
2024-06-13 20:57:14 +0300

    created the Django project and collated fields accordigly

---
+++ E_commerce/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import product 
+
+# Create your views here.
+def product_list(request):
+    products = product.objects.all()
+    context = {
+        'products': products,
+
+    }
+    return render(request,'E_commerce/product_list.html',context)
